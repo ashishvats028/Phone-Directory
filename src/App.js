3ead47373e175332a7ee00865bffa72a6165f551
ashishvats028 <ashishvats028@gmail.com>
2021-05-25 20:45:33 +0530

    Displayed Phone Directory

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, {Component} from 'react';
 
-class App extends Componen {
+class App extends Component {
   render() {
     return (
     <div>
-    
+     Phone Directory
     </div>
     );
  }
